7689ed273ec74881a30e9601b7fe0a064fdaffa4
unknown <bjones@LPTP-BOB.nwlq.local>
2015-09-29 17:11:17 -0400

    fixed header validation test

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -3,6 +3,7 @@ package com.mulesoft.training;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
@@ -13,9 +14,9 @@ public class HelloMavenTest extends FunctionalTestCase {
     }
     
     @Test
-    public void retrieveFlightsAddsAppropriateHeader throws Exception {
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
     	MuleEvent event = runFlow("retrieveFlights");
-    	String contentType = event.getMessage().getOutboundProperty("Content-­‐Type");
+    	String contentType = event.getMessage().getOutboundProperty("Content-Type");
     	assertEquals("application/json",contentType);
     }
     
